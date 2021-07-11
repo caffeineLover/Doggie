41a43eb269f8b1dca335901c3ac3da3f63420a90
caffeineLover <peter@dirac.org>
2021-07-11 15:46:22 -0400

    Laying the groundwork for really cool, functional, and smart pets.

---
+++ src/main/java/com/peter/doggie/AbstractPet.java
@@ -0,0 +1,25 @@
+package com.peter.doggie;
+
+import org.bukkit.DyeColor;
+import org.bukkit.Location;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class AbstractPet {
+    private UUID petID;
+    private UUID owner;
+    private String name;
+    private int level;
+    private int experience;
+    private Date birthday;
+    private Location location;
+    private Location home;
+    private DyeColor collar;  // FIXME: This might be specific for a Wolf.
+    //
+
+
+
+
+
+}
